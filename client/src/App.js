c60e4135f9aeea2b0717ad9167b0b89088db0d9b
Otis Singfield <singfieldotis@gmail.com>
2021-07-11 17:31:51 -0400

    set up login and signup

--- client/src/App.js
+++ client/src/App.js
@@ -17,12 +17,24 @@ import {
 } from "@apollo/client";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import { setContext } from '@apollo/client/link/context';
+
 const httpLink = createHttpLink({
   uri: "/graphql",
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
